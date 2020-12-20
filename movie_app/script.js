f99e36372dd0cf950e9ad5ef7bf6fc677c49a8fa
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-20 23:32:46 +0530

    Adding Movies to the dom

--- movie_app/script.js
+++ movie_app/script.js
@@ -1,23 +1,63 @@
 const API_URL =
-  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity&api_key=dc3f3eaa0d790bf6ce4bc56764597bab&page=1'
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=dc3f3eaa0d790bf6ce4bc56764597bab&page=3'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_URL =
+const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=dc3f3eaa0d790bf6ce4bc56764597bab&query="'
 
 const search = document.getElementById('search')
 const form = document.getElementById('form')
+const main = document.getElementById('main')
 //get initial movies
 getMovies(API_URL)
+
 async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
-  console.log(data.results)
+  showMovies(data.results)
 }
+function showMovies(movies) {
+  main.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+    movieEl.innerHTML = `
+        <img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+         ${overview}
+        </div>
+        `
+    main.appendChild(movieEl)
+  })
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green'
+  } else if (vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
+
   const searchTerm = search.value
+
   if (searchTerm && searchTerm !== '') {
-    getMovies(SEARCH_URL + searchTerm)
+    getMovies(SEARCH_API + searchTerm)
+
     search.value = ''
   } else {
     window.location.reload()
