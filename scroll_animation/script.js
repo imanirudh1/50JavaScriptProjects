8402c883abff46d09c7ca3eb0dae5b32e22611c8
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-25 20:35:18 +0530

    removed bottom animation

--- scroll_animation/script.js
+++ scroll_animation/script.js
@@ -6,8 +6,9 @@ function checkBoxes() {
   const trigger = (window.innerHeight / 5) * 4
   boxes.forEach((box) => {
     const boxTop = box.getBoundingClientRect().top
-    const boxBottom = box.getBoundingClientRect().bottom
-    if (boxTop < trigger && boxBottom > 0) {
+    // const boxBottom = box.getBoundingClientRect().bottom
+    // if (boxTop < trigger && boxBottom > 0) {
+    if (boxTop < trigger) {
       box.classList.add('show')
     } else {
       box.classList.remove('show')
